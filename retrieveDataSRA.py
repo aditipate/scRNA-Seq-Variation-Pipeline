d2ad261cbd6474ae18fec93cc4d4f8db5114697e
aditipate <apatel72@luc.edu>
2021-06-07 16:23:09 -0500

    removed -I parameter

--- retrieveDataSRA.py
+++ retrieveDataSRA.py
@@ -12,7 +12,7 @@ def getSRAdata(SRRs):
         if(SRR[-1] == '3'):
             SRR_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-15/' + SRR + '/' + SRR + '.1'
             wget_SRR = 'wget' + ' ' + SRR_data_address
-            fastq_dump_SRR = 'fastq-dump -I --split-files' + ' ' + SRR + '.1'
+            fastq_dump_SRR = 'fastq-dump --split-files' + ' ' + SRR + '.1'
             os.system(wget_SRR)                                                #retrieve SRA data using wget command
             os.system(fastq_dump_SRR)                                          #uncompress data and convert to paired-end fastq files using fastq-dump command
 
@@ -20,7 +20,7 @@ def getSRAdata(SRRs):
         else:
             SRR_data_address = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos3/sra-pub-run-20/' + SRR + '/' + SRR + '.1'
             wget_SRR = 'wget' + ' ' + SRR_data_address
-            fastq_dump_SRR = 'fastq-dump -I --split-files' + ' ' + SRR + '.1'
+            fastq_dump_SRR = 'fastq-dump --split-files' + ' ' + SRR + '.1'
             os.system(wget_SRR)                                                #retrieve SRA data using wget command
             os.system(fastq_dump_SRR)                                          #uncompress data and convert to paired-end fastq files using fastq-dump command
 
