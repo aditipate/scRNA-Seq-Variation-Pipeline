ffb04ce80f6713340140536cf96de098c9e0d77c
aditipate <apatel72@luc.edu>
2021-06-07 16:24:43 -0500

    removed genome function - redundant

--- testretrieveDataSRA.py
+++ testretrieveDataSRA.py
@@ -34,18 +34,3 @@ def getSRAdata(SRRs):
             os.system(fastq_dump_SRR)                                          #uncompress data and convert to paired-end fastq files using fastq-dump command
 
     os.chdir(current_path)                                                     #change to current directory
-
-
-#function to retrieve mouse reference genome index
-def getRefGenome():
-    current_path = os.getcwd()                                                  #create and change to mouse genome folder directory
-    folder = "/mouse_genome"
-    os.mkdir(current_path + folder)
-    os.chdir(current_path + folder)
-
-    wget_genome = 'wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-mm10-2020-A.tar.gz'  #retrieve cell ranger mouse genome
-    decompress_genome = 'tar -zxvf refdata-gex-mm10-2020-A.tar.gz'
-    os.system(wget_genome)
-    os.system(decompress_genome)
-
-    os.chdir(current_path)                                                       #change to current directory
